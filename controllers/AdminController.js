11068519c67acc5cdc929f3b792e1cdb52bf4af9
Nolen Robinson <149610446+nrobinson4@users.noreply.github.com>
2025-04-22 22:29:09 -0500

    Delete controllers directory

--- controllers/AdminController.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const mongoClient = require("../services/MongoClientService");
-const APIResponse = require("../DTOs/APIResponse");
-
-router.use(function(req, res, next) { next() });
-
-module.exports = router;
